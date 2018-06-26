c587eea14ce18146e1c478c39f5fca38367e99a8
Pico143 <pico19942@gmail.com>
2018-06-26 11:56:09 +0200

    implemented num of cards

--- src/com/codecool/klondike/Pile.java
+++ src/com/codecool/klondike/Pile.java
@@ -40,8 +40,7 @@ public class Pile extends Pane {
     }
 
     public int numOfCards() {
-        //TODO
-        return 1;
+        return this.getCards().size();
     }
 
     public boolean isEmpty() {
