bdc9ab1a46ddfa7e8574cf2d04923dcd1157606e
Witold Tafel <witold.tafel@gmail.com>
2018-06-28 03:25:15 +0200

    placeablePiles created in initPiles, minor bugs in validation

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -6,41 +6,129 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 
 import java.util.*;
 
 public class Game extends Pane {
 
+    // Number of piles
+    private static final int FOUNDATIONS = Suit.values().length;
+    private static final int TABLEAUS = 7;
+    private static double STOCK_GAP = 1;
+    private static double FOUNDATION_GAP = 0;
+    private static double TABLEAU_GAP = 30;
+    private static int currentBackgroundThemeNumber = 0;
+    private static int allBgThemes = 5; // = numOfFiles() method if worked
+    private static int currentCardThemeNumber = 0;
+    private static int allCardThemes = 2;
     private List<Card> deck = new ArrayList<>();
-
     // Piles
     private Pile stockPile;
     private Pile discardPile;
     private List<Pile> foundationPiles = FXCollections.observableArrayList();
     private List<Pile> tableauPiles = FXCollections.observableArrayList();
-
-
-    // Number of piles
-    private static final int FOUNDATIONS = Suit.values().length;
-    private static final int TABLEAUS = 7;
-
+    private List<Pile> placeablePiles = FXCollections.observableArrayList();
     private double dragStartX, dragStartY;
     private List<Card> draggedCards = FXCollections.observableArrayList();
+    private EventHandler<MouseEvent> onMouseClickedHandler = e -> {
+        Card card = (Card) e.getSource();
+        if (e.getClickCount() == 2 && !e.isConsumed() && !card.isFaceDown()) {
+            e.consume();
 
-    private static double STOCK_GAP = 1;
-    private static double FOUNDATION_GAP = 0;
-    private static double TABLEAU_GAP = 30;
+            for (Pile pile : foundationPiles) {
+                Card topCard = pile.getTopCard();
+                if (topCard == null) {
+                    if (card.getRank().equals(Rank.Ace)) card.moveToPile(pile);
+                    break;
+                } else {
+                    int topRank = topCard.getRank().getValue();
+                    int cardRank = card.getRank().getValue();
+                    if (topCard.getSuit().equals(card.getSuit()) && topRank == cardRank - 1) {
+                        card.moveToPile(pile);
+                        break;
+                    }
+                }
+            }
+        } else {
+            if (card.getContainingPile().getPileType() == Pile.PileType.STOCK) {
+                card.moveToPile(discardPile);
+                card.flip();
+                card.setMouseTransparent(false);
+                System.out.println("Placed " + card + " to the waste.");
+            }
+        }
+    };
+    private EventHandler<MouseEvent> stockReverseCardsHandler = e -> {
+        refillStockFromDiscard();
+    };
+    private EventHandler<MouseEvent> onMousePressedHandler = e -> {
+        dragStartX = e.getSceneX();
+        dragStartY = e.getSceneY();
+    };
 
-    private static int currentBackgroundThemeNumber = 0;
-    private static int allBgThemes = 5; // = numOfFiles() method if worked
-    private static int currentCardThemeNumber = 0;
-    private static int allCardThemes = 2;
+
+    // This method should count number of files in the directory, but path contains user name
+//    private int numOfFiles(){
+//        File dir = new File("/home/mariusz/IdeaProjects/oop-solitaire/resources/table");
+//        int numberOfSubfolders = 0;
+//        File listDir[] = dir.listFiles();
+//        return listDir.length;
+//    }
+    private EventHandler<MouseEvent> onMouseDraggedHandler = e -> {
+        Card card = (Card) e.getSource();
+        Pile activePile = card.getContainingPile();
+        if (activePile.getPileType() == Pile.PileType.STOCK)
+            return;
+        if (card.isFaceDown()) {
+            return;
+        }
+        double offsetX = e.getSceneX() - dragStartX;
+        double offsetY = e.getSceneY() - dragStartY;
+
+        draggedCards.clear();
+        ListIterator<Card> it = activePile.getCards().listIterator();
+        Card currentCard = it.hasNext() ? it.next() : null;
+        while (currentCard != null && !card.equals(currentCard)) {
+            currentCard = it.next();
+        }
+        draggedCards.add(card);
+        while (it.hasNext()) {
+            currentCard = it.next();
+            draggedCards.add(currentCard);
+        }
+
+        card.getDropShadow().setRadius(20);
+        card.getDropShadow().setOffsetX(10);
+        card.getDropShadow().setOffsetY(10);
+
+        card.toFront();
+        card.setTranslateX(offsetX);
+        card.setTranslateY(offsetY);
+    };
+    private EventHandler<MouseEvent> onMouseReleasedHandler = e -> {
+        if (draggedCards.isEmpty())
+            return;
+
+        Card card = (Card) e.getSource();
+        Pile pile = getValidIntersectingPile(card, placeablePiles);
+        //TODO
+        if (pile != null) {
+            handleValidMove(card, pile);
+        } else {
+            draggedCards.forEach(MouseUtil::slideBack);
+            draggedCards = FXCollections.observableArrayList();
+        }
+    };
+
+    public Game() {
+        setThemeButton();
+        deck = CardManager.createNewDeck();
+        shuffleDeck();
+        initPiles();
+        dealCards();
+        flipTopCards();
+    }
 
     public void setThemeButton() {
         // Background theme:
@@ -86,114 +174,9 @@ public class Game extends Pane {
         }
     }
 
-
-    // This method should count number of files in the directory, but path contains user name
-//    private int numOfFiles(){
-//        File dir = new File("/home/mariusz/IdeaProjects/oop-solitaire/resources/table");
-//        int numberOfSubfolders = 0;
-//        File listDir[] = dir.listFiles();
-//        return listDir.length;
-//    }
-
-    private EventHandler<MouseEvent> onMouseClickedHandler = e -> {
-        Card card = (Card) e.getSource();
-        if (e.getClickCount() == 2 && !e.isConsumed() && !card.isFaceDown()) {
-            e.consume();
-
-            for (Pile pile : foundationPiles) {
-                Card topCard = pile.getTopCard();
-                if (topCard == null) {
-                    if (card.getRank().equals(Rank.Ace)) card.moveToPile(pile);
-                    break;
-                } else {
-                    int topRank = topCard.getRank().getValue();
-                    int cardRank = card.getRank().getValue();
-                    if (topCard.getSuit().equals(card.getSuit()) && topRank == cardRank - 1) {
-                        card.moveToPile(pile);
-                        break;
-                    }
-                }
-            }
-        } else {
-            if (card.getContainingPile().getPileType() == Pile.PileType.STOCK) {
-                card.moveToPile(discardPile);
-                card.flip();
-                card.setMouseTransparent(false);
-                System.out.println("Placed " + card + " to the waste.");
-            }
-        }
-    };
-
-    private EventHandler<MouseEvent> stockReverseCardsHandler = e -> {
-        refillStockFromDiscard();
-    };
-
-    private EventHandler<MouseEvent> onMousePressedHandler = e -> {
-        dragStartX = e.getSceneX();
-        dragStartY = e.getSceneY();
-    };
-
-    private EventHandler<MouseEvent> onMouseDraggedHandler = e -> {
-        Card card = (Card) e.getSource();
-        Pile activePile = card.getContainingPile();
-        if (activePile.getPileType() == Pile.PileType.STOCK)
-            return;
-        if (card.isFaceDown()) {
-            return;
-        }
-        double offsetX = e.getSceneX() - dragStartX;
-        double offsetY = e.getSceneY() - dragStartY;
-
-        draggedCards.clear();
-        ListIterator<Card> it = activePile.getCards().listIterator();
-        Card currentCard = it.hasNext() ? it.next() : null;
-        while (currentCard != null && !card.equals(currentCard)) {
-            currentCard = it.next();
-        }
-        draggedCards.add(card);
-        while (it.hasNext()) {
-            currentCard = it.next();
-            draggedCards.add(currentCard);
-        }
-
-        card.getDropShadow().setRadius(20);
-        card.getDropShadow().setOffsetX(10);
-        card.getDropShadow().setOffsetY(10);
-
-        card.toFront();
-        card.setTranslateX(offsetX);
-        card.setTranslateY(offsetY);
-    };
-
-    private EventHandler<MouseEvent> onMouseReleasedHandler = e -> {
-        if (draggedCards.isEmpty())
-            return;
-
-        Card card = (Card) e.getSource();
-        Pile pile = getValidIntersectingPile(card, foundationPiles);
-        //TODO
-        if (pile != null) {
-           handleValidMove(card, pile);
-        }
-        else {
-            draggedCards.forEach(MouseUtil::slideBack);
-            draggedCards = FXCollections.observableArrayList();
-        }
-
-    };
-
     public boolean isGameWon() {
         //TODO
         return false;
-    }
-
-    public Game() {
-        setThemeButton();
-        deck = CardManager.createNewDeck();
-        shuffleDeck();
-        initPiles();
-        dealCards();
-        flipTopCards();
     }
 
     public void addMouseEventHandlers(Card card) {
@@ -212,10 +195,10 @@ public class Game extends Pane {
         //TODO
         if (destPile.getPileType() == Pile.PileType.TABLEAU) {
             if (destPile.isEmpty() && card.getRank().equals(Rank.King)) return true;
-            else return CardManager.checkIfLowerRankOpposingColor(card, destPile);
+            else if (!destPile.isEmpty()) return CardManager.checkIfLowerRankOpposingColor(card, destPile);
         } else if (destPile.getPileType() == Pile.PileType.FOUNDATION) {
             if (destPile.isEmpty() && card.getRank() == Rank.Ace) return true;
-            else if(!destPile.isEmpty()) return CardManager.checkIfHigherRankSameSuit(card, destPile);
+            else if (!destPile.isEmpty()) return CardManager.checkIfHigherRankSameSuit(card, destPile);
         }
         return false;
     }
@@ -275,6 +258,7 @@ public class Game extends Pane {
             foundationPile.setLayoutY(20);
             foundationPiles.add(foundationPile);
             getChildren().add(foundationPile);
+            placeablePiles.add(foundationPile);
         }
         for (int i = 0; i < TABLEAUS; i++) {
             Pile tableauPile = new Pile(Pile.PileType.TABLEAU, "Tableau " + i, TABLEAU_GAP);
@@ -283,6 +267,7 @@ public class Game extends Pane {
             tableauPile.setLayoutY(275);
             tableauPiles.add(tableauPile);
             getChildren().add(tableauPile);
+            placeablePiles.add(tableauPile);
         }
     }
 
